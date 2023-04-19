4f99a40fb6a0412144e676e87698c93f2aedc095
Justin Huynh <juh079@ucsd.edu>
2023-04-18 17:44:35 -0700

    eval and attention

---
+++ generate_attention_maps.py
@@ -0,0 +1,205 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from torchvision import models, transforms
+import numpy as np
+from dataset import ImageDataset, ImageRNFLDataset, ImageRNFLSeparateDataset, ImageRNFLVFDataset
+import pandas as pd
+import sklearn
+from sklearn.metrics import roc_auc_score, confusion_matrix, roc_curve
+from vit import ViT
+from vit_mmodal import ViT_rnfl, ViT_rnfl_vf
+from recorder import Recorder
+import matplotlib.pyplot as plt
+from scipy.ndimage import zoom
+from datetime import datetime
+
+# Define the normalization parameters
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225]
+data_transforms = transforms.Compose([
+    transforms.Resize((224, 224)), 
+    transforms.ToTensor(),
+    transforms.Normalize(mean, std)
+])
+# Define the inverse normalization transform
+inv_normalize = transforms.Normalize(
+    mean=[-m/s for m, s in zip(mean, std)],
+    std=[1/s for s in std]
+)
+
+model_path = "________.pth"
+
+df_oct = pd.read_csv("_______.csv", index_col=0)
+df_oct_test = df_oct[df_oct["cohort"] == "test"]
+test_paths = ["/projects/ps-visres-group/raw-bscans/{}".format(path) for path in df_oct_test["path"]]
+
+# rnfl thickness for multimodal
+test_rnfl_paths = ["../metadata/rnfl_thickness/{}.npy".format(path.split("/")[1][:-4]) for path in df_oct_test["path"]]
+
+# vf for multimodal
+test_vf_paths = ["../metadata/visual_fields/{}.npy".format(row["IDEYE"] + "_" + row["date_first_progression"]) for _,row in df_oct_test.iterrows()]
+
+label_map = {"Healthy":0.0, "GVFD & GON":1.0, "GVFD":1.0}
+test_labels = [label_map[x] for x in df_oct_test["glaucoma"]]
+
+# if not multimodal
+# test_dataset = ImageDataset(test_paths, test_labels, data_transforms)
+
+# if multimodal
+# test_dataset = ImageRNFLDataset(test_paths, test_rnfl_paths, test_labels, data_transforms)
+
+# if multimodal_custom
+# test_dataset = ImageRNFLSeparateDataset(test_paths, test_rnfl_paths, test_labels, data_transforms)
+
+# if multimodal rnfl and vf
+test_dataset = ImageRNFLVFDataset(test_paths, test_rnfl_paths, test_vf_paths, test_labels, data_transforms)
+
+test_loader = DataLoader(test_dataset, batch_size=1, shuffle=True, num_workers=1) # shuffle false!
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# model = ViT(
+#     image_size = 224,
+#     patch_size = 16,
+#     num_classes = 1,
+#     dim = 768,
+#     depth = 12,
+#     heads = 12,
+#     mlp_dim = 3072,
+#     dropout = 0.0,
+#     emb_dropout = 0.0,
+# )
+# model.mlp_head.append(torch.nn.Sigmoid())
+
+# model = ViT_rnfl(
+#     image_size = 224,
+#     patch_size = 16,
+#     num_classes = 1,
+#     dim = 768,
+#     depth = 12,
+#     heads = 12,
+#     mlp_dim = 3072,
+#     dropout = 0.0,
+#     emb_dropout = 0.0,
+#     rnfl_len = 768,
+#     num_segments = 128,
+# )
+# model.mlp_head.append(torch.nn.Sigmoid())
+
+# custom mmodal vit with rnfl and vf
+model = ViT_rnfl_vf(
+    image_size = 224,
+    patch_size = 16,
+    num_classes = 1,
+    dim = 768,
+    depth = 12,
+    heads = 12,
+    mlp_dim = 3072,
+    dropout = 0.0,
+    emb_dropout = 0.0,
+    rnfl_len = 768,
+    rnfl_num_segments = 128,
+    vf_len = 156,
+    vf_num_segments = 39,
+)
+model.mlp_head.append(torch.nn.Sigmoid())
+
+
+# model = models.vit_b_16(image_size=224)
+# num_ftrs = model.heads.head.in_features
+# model.heads = torch.nn.Sequential(
+#     torch.nn.Linear(in_features=num_ftrs,out_features=1),   
+#     torch.nn.Sigmoid()
+# )
+# model = models.resnet50()
+# num_ftrs = model.fc.in_features
+# model.fc = torch.nn.Sequential(
+#     torch.nn.Linear(in_features=num_ftrs,out_features=1),   
+#     torch.nn.Sigmoid()
+# )
+model.load_state_dict(torch.load(model_path))
+model = model.to(device)
+model.eval()
+model = Recorder(model, device)
+
+threshold = 0.5
+running_corrects = 0
+all_preds_logits = []
+all_preds = []
+all_labels = []
+# Iterate over data.
+for inputs, labels in test_loader:
+    if type(inputs) == list:
+        inputs_list = []
+        for x in inputs:
+            inputs_list.append(x.to(device))
+        inputs = inputs_list
+    else:    
+        inputs = inputs.to(device)
+    labels = labels.type(torch.LongTensor)
+    labels = labels.to(device)
+
+    outputs, attns = model(inputs)
+
+    # this will generate the attention map for a single image. To get avg, save all in list and average.
+    #------------------------------------------------------------------------
+    print(outputs, attns.shape) 
+    # you need to figure out the size of your entire attention matrix and select numbers corresponding to oct, vf, etc.
+    curr_attn = torch.mean(attns[0,0,:,:,1:197],1).reshape(12,14,14) # 1:197,197:393,393:589,589:785, mean over all positions, layers
+    curr_attn = nn.functional.interpolate(curr_attn.unsqueeze(0), scale_factor=(16,16), mode="bilinear")[0] # scale up the nxn attention map by 16 to original res
+    curr_attn = torch.mean(curr_attn,0).cpu().numpy() # mean over all heads
+    plt.gcf().set_dpi(300)
+    curr_inp = inv_normalize(inputs[0]) # original input image, remove the normalization
+    plt.imshow(np.rollaxis(curr_inp[0].cpu().numpy(), 0, 3)) # plot original input image
+    plt.imshow(curr_attn, alpha=0.8) # overlay the attention
+    plt.savefig("sample_attn_glaucoma")
+    # --------------------------------------------------------------------
+
+    preds = torch.where(outputs > threshold, 1., 0.)
+
+    running_corrects += torch.sum(preds == labels.data)
+
+    all_preds.append(preds.item())
+    all_preds_logits.append(outputs.item())
+    all_labels.append(labels.item())
+
+test_acc = running_corrects.double() / len(test_dataset)
+print("test accuracy: ", test_acc.item())
+auroc = roc_auc_score(all_labels, all_preds_logits)
+print("auroc: ", auroc)
+
+def Find_Optimal_Cutoff(target, predicted):
+    """ Find the optimal probability cutoff point for a classification model related to event rate
+    Parameters
+    ----------
+    target : Matrix with dependent or target data, where rows are observations
+
+    predicted : Matrix with predicted data, where rows are observations
+
+    Returns
+    -------     
+    list type, with optimal cutoff value
+        
+    """
+    fpr, tpr, threshold = roc_curve(target, predicted)
+    i = np.arange(len(tpr)) 
+    roc = pd.DataFrame({'tf' : pd.Series(tpr-(1-fpr), index=i), 'threshold' : pd.Series(threshold, index=i)})
+    roc_t = roc.iloc[(roc.tf-0).abs().argsort()[:1]]
+
+    return list(roc_t['threshold']) 
+
+# Find optimal probability threshold
+threshold = Find_Optimal_Cutoff(all_labels, all_preds_logits)[0]
+# print threshold
+# [0.31762762459360921]
+
+# Find prediction to the dataframe applying threshold
+all_preds = list(map(lambda x: 1 if x > threshold else 0, all_preds_logits))
+
+tn, fp, fn, tp = confusion_matrix(all_labels, all_preds).ravel()
+sensitivity = tp / (tp+fn)
+specificity = tn / (tn+fp)
+print("sensitivity: {}, specificity: {}".format(sensitivity, specificity))
+
+np.save("{}_preds".format(model_path[:-4]), {"labels":all_labels, "all_preds_logits":all_preds_logits})
---
+++ recorder.py
@@ -0,0 +1,69 @@
+from functools import wraps
+import torch
+from torch import nn
+
+from vit_mmodal import Attention
+
+def find_modules(nn_module, module_type):
+    # print("modules: ")
+    # for module in nn_module.modules():
+    #     print(module)
+    #     print(isinstance(module, type))
+    #     print(type(module), module_type)
+    #     print("*" * 10)
+    return [module for module in nn_module.modules() if isinstance(module, module_type)]
+
+class Recorder(nn.Module):
+    def __init__(self, vit, device = None):
+        super().__init__()
+        self.vit = vit
+
+        self.data = None
+        self.recordings = []
+        self.hooks = []
+        self.hook_registered = False
+        self.ejected = False
+        self.device = device
+
+    def _hook(self, _, input, output):
+        self.recordings.append(output.clone().detach())
+
+    def _register_hook(self):
+        modules = find_modules(self.vit.transformer, Attention)
+        # print("modules: ", modules)
+        for module in modules:
+            handle = module.attend.register_forward_hook(self._hook)
+            self.hooks.append(handle)
+        self.hook_registered = True
+
+    def eject(self):
+        self.ejected = True
+        for hook in self.hooks:
+            hook.remove()
+        self.hooks.clear()
+        return self.vit
+
+    def clear(self):
+        self.recordings.clear()
+
+    def record(self, attn):
+        recording = attn.clone().detach()
+        self.recordings.append(recording)
+
+    def forward(self, img):
+        assert not self.ejected, 'recorder has been ejected, cannot be used anymore'
+        self.clear()
+        if not self.hook_registered:
+            self._register_hook()
+            # print(self.hook_registered, "registered hook!")
+
+        pred = self.vit(img)
+
+        # move all recordings to one device before stacking
+        # print("recordings: ", self.recordings)
+        target_device = self.device if self.device is not None else img.device
+        recordings = tuple(map(lambda t: t.to(target_device), self.recordings))
+        # recordings = self.recordings
+
+        attns = torch.stack(recordings, dim = 1) if len(recordings) > 0 else None
+        return pred, attns
